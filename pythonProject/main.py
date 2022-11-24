e5779ce27ec04bda1d94f7f5fdd221dba6069914
rajijo <joseph.raji@outlook.fr>
2022-11-24 13:10:41 +0200

    Version-1

---
+++ pythonProject/main.py
@@ -0,0 +1,8 @@
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print "Hiii, {0}".format(name)  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+print_hi("hello")
